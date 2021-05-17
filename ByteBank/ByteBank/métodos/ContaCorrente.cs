70d124a46938fc80ee7ee16a5d9eb7e25422e2a7
Matheus Henrique Szoke La Motta <Matheusszoke@gmail.com>
2021-05-17 14:35:52 -0300

    update in system

---
+++ ByteBank/ByteBank/métodos/ContaCorrente.cs
@@ -0,0 +1,118 @@
+﻿// using _005_ByteBank;
+
+namespace _007_ByteBank
+{
+    public class ContaCorrente
+    {
+        public static double TaxaOperacao { get; private set; }
+
+
+        public Cliente Titular { get; set; }
+
+        public static int SetDeContasCriadas { get; private set; }
+
+        public static int GetTotalDeContasCriadas()
+        {
+            return SetDeContasCriadas;
+        }
+
+        private int _agencia;
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+            }
+        }
+        public int Numero { get; set; }
+
+
+        public int agencia;
+        public int numero;
+        private double _saldo = 100;
+
+
+        public double Saldo
+        {
+            get //obter
+            {
+                return _saldo;
+            }
+            set //definir
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
+
+        public void SetSaldo(double saldo)
+        {
+
+        }
+
+        public double GetSaldo()
+        {
+            return _saldo;
+        }
+
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+
+            TaxaOperacao = 30 / SetDeContasCriadas;
+
+            SetDeContasCriadas++;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+
+
+    }
+
+}
+
+
+
+
---
+++ ByteBank/ByteBank/métodos/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(7480, 874150);
+
+            Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            Console.ReadLine();
+        }
+
+        // Teste com a cadeia de chamada:
+        // Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(2);
+            TestaDivisao(0);
+        }
+
+        private static int TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+
+            if(resultado == -2)
+            {
+                return -2;
+            }
+
+            if (resultado == -1)
+            {
+                Console.WriteLine("Não é possivel fazer divisão por 0");
+            }
+            else
+            {
+                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+
+            }
+            return 0;
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            if (divisor == 0)
+            {
+                return -1;
+            }
+
+            return numero / divisor;
+        }
+    }
+}
